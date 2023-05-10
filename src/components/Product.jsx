b9b55e30ff44aa081bea09d8c4b05bb436ec990e
Victor Chan <vctrchn@hotmail.ca>
2023-05-09 20:28:13 -0400

    working on product cards

--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -1,7 +1,24 @@
 import React from "react";
+import PropTypes from "prop-types";
 
-function Product() {
-  return <div>Hi</div>;
+function Product(props) {
+  const { product } = props;
+
+  Product.propTypes = {
+    product: PropTypes.shape({
+      category: PropTypes.string.isRequired,
+      productName: PropTypes.string.isRequired,
+      cost: PropTypes.number.isRequired,
+      id: PropTypes.string.isRequired,
+    }).isRequired,
+  };
+
+  return (
+    <div id={product.id}>
+      {product.productName}
+      {product.cost}
+    </div>
+  );
 }
 
 export default Product;
--- src/components/ProductCategory.jsx
+++ src/components/ProductCategory.jsx
@@ -1,13 +1,72 @@
 import React from "react";
-import { useParams, useState } from "react-router-dom";
+import { useParams } from "react-router-dom";
 import "../styles/ProductCategory.css";
+import { uniqueId } from "lodash";
+import Product from "./Product";
 
 function ProductCategory() {
   const { productCategory } = useParams();
 
-  const { product, setProduct } = useState([]);
+  const productList = [
+    {
+      category: "cpus",
+      productName: "Ryzen",
+      cost: 300,
+      id: uniqueId(),
+    },
+    {
+      category: "cpu-coolers",
+      productName: "Corsair",
+      cost: 50,
+      id: uniqueId(),
+    },
+    {
+      category: "motherboards",
+      productName: "Asus",
+      cost: 150,
+      id: uniqueId(),
+    },
+    {
+      category: "cases",
+      productName: "NZXT",
+      cost: 100,
+      id: uniqueId(),
+    },
+    {
+      category: "memory",
+      productName: "Munchkin",
+      cost: 120,
+      id: uniqueId(),
+    },
+    {
+      category: "video-card",
+      productName: "Geforce",
+      cost: 700,
+      id: uniqueId(),
+    },
+    {
+      category: "power-supplies",
+      productName: "Evga",
+      cost: 200,
+      id: uniqueId(),
+    },
+    {
+      category: "storage",
+      productName: "Samsung",
+      cost: 130,
+      id: uniqueId(),
+    },
+  ];
 
-  return <div className="product-category">{productCategory}</div>;
+  return (
+    <div className="product-category">
+      {productList
+        .filter((product) => product.category === productCategory)
+        .map((filteredProduct) => (
+          <Product product={filteredProduct} key={filteredProduct.id} />
+        ))}
+    </div>
+  );
 }
 
 export default ProductCategory;
